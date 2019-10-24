88f88c8d0dc294076df9d179f11c62494d07e3bd
Gina Graci <ginagraci92@gmail.com>
2019-10-24 02:14:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,4 @@
-class Dog 
-  def initialize(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-   
-   def breed
-     @breed
-   end
-    
-end
+
 
 fido = Dog.new
 fido.name = "Fido"
